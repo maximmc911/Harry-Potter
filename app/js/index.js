8b98fe9d80bbb6609fe6c128b9f7b2c0351a512c
max <89175221282@mail.ru>
2023-06-13 17:57:32 +0300

    make textHeader js

--- app/js/index.js
+++ app/js/index.js
@@ -1,6 +1,15 @@
+
+// Header text
+
+import { writeHeaderText  } from "./module/nameHeader.js";
+writeHeaderText();
+
+ // Main card
 import { MakeCard } from "./module/ReserveFile.js";
 MakeCard();
 
 
 // ! Проверки!!!!!!!
-
+// Timer
+import { PresentTimer } from "./module/timer.js";
+PresentTimer()
